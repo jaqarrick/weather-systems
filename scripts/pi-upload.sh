a92b59c2ce90ec9c5cab63d99cbf1995119716e8
jaqarrick <jcarrick@protonmail.com>
2021-06-04 16:49:01 -0700

    Add separate upload script for pi zero

---
+++ scripts/pi-upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# arduino-cli compile --fqbn arduino:avr:uno /home/pi/Web/weather-systems/controller/controller
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/Web/weather-systems/controller/controller
