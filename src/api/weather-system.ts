6a296f2ef8a9b78a8bd5a205d9580656ee8fe569
jaqarrick <jcarrick@protonmail.com>
2021-07-11 22:26:54 -0700

    Add simple frontend interface for viewing / changing speed and city

--- src/api/weather-system.ts
+++ src/api/weather-system.ts
@@ -5,98 +5,135 @@ const SerialPort = require("serialport");
 const apiKey = fs.readFileSync(path.join("apikey.txt"), { encoding: "utf8" });
 
 class WeatherSystem {
-  public city: string;
-  public port: typeof SerialPort;
-  private parser: typeof SerialPort.parsers;
-  private delay: number;
-  private interval: ReturnType<typeof setInterval>;
-  private isPortOpen: Promise<boolean>
+    public city: string;
+    public port: typeof SerialPort;
+    public windSpeed: number | null;
+    private parser: typeof SerialPort.parsers;
+    private delay: number;
+    private interval: ReturnType<typeof setInterval>;
+    private isPortOpen: Promise<boolean>;
+    private io: any
 
-  constructor(pathToSerialPort: string | null, city: string = "Chicago") {
-    this.city = city;
-    console.log("Fetching weather for ", this.city)
-    console.log("Linking arduino from port", pathToSerialPort)
-    this.port = pathToSerialPort
-      ? new SerialPort(pathToSerialPort, {
-          baudRate: 9600,
-          dataBits: 8,
-          parity: "none",
-          stopBits: 1,
-          flowControl: false,
-        })
-      : null;
+    constructor(
+        pathToSerialPort: string | null,
+        city: string = "Curitiba",
+        io,
+        shouldConnectPort: boolean = true
+    ) {
+        this.io = io;
+        this.city = city;
+        this.windSpeed = null;
+        console.log("Fetching weather for ", this.city);
+        console.log(
+            pathToSerialPort
+                ? `Linking arduino from port", ${pathToSerialPort}`
+                : "No port specified "
+        );
+        this.port =
+            pathToSerialPort && shouldConnectPort
+                ? new SerialPort(pathToSerialPort, {
+                      baudRate: 9600,
+                      dataBits: 8,
+                      parity: "none",
+                      stopBits: 1,
+                      flowControl: false,
+                  })
+                : null;
 
-    const parsers = SerialPort.parsers;
+        //Whenever someone connects this gets executed
+        io?.on("connection", this.socketConnectionCallback);
 
-    this.parser = new parsers.Readline({
-      deminiter: "\r\n",
-    });
+        const parsers = SerialPort.parsers;
 
-    this.parser.on("data", (data) => {
-      console.log(data);
-    });
+        this.parser = new parsers.Readline({
+            deminiter: "\r\n",
+        });
 
-    // How often a request is sent to the weather api
-    // Should be more than 10sec / 10,000ms
-    this.delay = 30000;
+        this.parser?.on("data", (data) => {
+            console.log(data);
+        });
 
-    this.isPortOpen = new Promise(res => {
+        // How often a request is sent to the weather api
+        // Should be more than 10sec / 10,000ms
+        this.delay = 30000;
 
-      this.port.on("open", ()=> {
-        console.log("Port is open")
-        res(true)
-      })
-
-    })
-  }
-
-  getWindSpeed = async () => {
-    try {
-      const response = await nodeFetch(
-        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${this.city}&aqi=no`
-      );
-
-      const json = await response.json();
-
-      return json.current.wind_mph ?? null;
-    } catch (err) {
-      console.log(err);
+        this.isPortOpen = new Promise((res) => {
+            this.port?.on("open", () => {
+                console.log("Port is open");
+                res(true);
+            });
+        });
     }
-  };
 
-  translateWindSpeed = (km: number): Buffer => {
-    const windSpeedInteger = Math.floor(km);
-    const fromArray = [];
-    if (windSpeedInteger < 1 || !windSpeedInteger) fromArray.push(0);
-    else if (windSpeedInteger <= 10) fromArray.push(windSpeedInteger);
-    else if (windSpeedInteger > 10) fromArray.push(10);
-    else fromArray.push(0);
+    socketConnectionCallback = (socket) => {
+      console.log("A user connected");
+      console.log(this.city)
+      console.log(this.windSpeed)
+      socket.on("send city", city => {
+        this.city = city;
+      })
+      socket.on("disconnect", function () {
+        console.log("A user disconnected");
+      });
+    }
+    getWindSpeed = async () => {
+        try {
+            const response = await nodeFetch(
+                `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${this.city}&aqi=no`
+            );
 
-    const buf = Buffer.from([fromArray[0]]);
-    // const buf = Buffer.from([0]);
+            const json = await response.json();
 
+            return json.current.wind_mph ?? null;
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    return buf;
-  };
+    translateWindSpeed = (km: number): Buffer => {
+        const windSpeedInteger = Math.floor(km);
+        const fromArray = [];
+        if (windSpeedInteger < 1 || !windSpeedInteger) fromArray.push(0);
+        else if (windSpeedInteger <= 10) fromArray.push(windSpeedInteger);
+        else if (windSpeedInteger > 10) fromArray.push(10);
+        else fromArray.push(0);
 
-  callback = () =>
-    this.getWindSpeed().then((speed) => {
-      console.log(`The wind speed in ${this.city} is ${speed}km.`);
+        const buf = Buffer.from([fromArray[0]]);
+        // const buf = Buffer.from([0]);
 
-      const buf = this.translateWindSpeed(speed);
-      console.log("Sending buffer: ", buf, " to Serial Port");
-      this.port.write(buf);
+        return buf;
+    };
 
-    });
+    callback = () =>
+        this.getWindSpeed().then((speed) => {
 
-  start = () => {
-    this.isPortOpen.then(() => {
-      this.callback()
-      this.interval = setInterval(this.callback, this.delay);
-    });
-  };
+            console.log(`The wind speed in ${this.city} is ${speed}km.`);
+            this.io.emit("send speed", {
+              city: this.city,
+              speed
+            });
 
-  stop = () => clearInterval(this.interval);
+            this.windSpeed = speed
+            const buf = this.translateWindSpeed(speed);
+            if (this.port) {
+                this.port.write(buf);
+                console.log("Sending buffer: ", buf, " to Serial Port");
+            }
+        });
+
+    start = () => {
+        if (this.port) {
+            this.isPortOpen.then(() => {
+                this.callback();
+                this.interval = setInterval(this.callback, this.delay);
+            });
+        } else {
+            this.callback();
+            this.interval = setInterval(this.callback, this.delay);
+        }
+    };
+
+    stop = () => clearInterval(this.interval);
 }
 
 module.exports = WeatherSystem;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 (function () {
+
+  const app = require('express')();
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http);
+  const path = require("path");
+
+  app.get('/', function(req, res) {
+     res.sendfile(path.join(__dirname, "public", 'index.html'))
+  });
+  
+  http.listen(1312, function() {
+     console.log('listening on 1312');
+  });
+
   const WeatherSystem = require("./api/weather-system");
-  const server = require("./server");
   const fs = require("fs");
 
   // running yarn launch should print the correct port.txt file
@@ -12,7 +25,7 @@
       .trim();
 
   
-  const weatherSystem = new WeatherSystem(getPathToSerialPort(), "Seattle");
+  const weatherSystem = new WeatherSystem(getPathToSerialPort(), "Seattle", io, false);
 
   weatherSystem.start();
 
