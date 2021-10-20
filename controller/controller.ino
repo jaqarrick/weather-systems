d4c0bbdd21278b7f1bb34a0098201cb767bb1884
jaqarrick <jcarrick@protonmail.com>
2021-10-19 20:38:31 -0700

    Tweaks to controller

--- controller/controller.ino
+++ controller/controller.ino
@@ -8,14 +8,16 @@ int pos = 0;
 int incomingData = 0;
 int speed = 0;
 int intensity = 0;
-int centerPos = 20;
+int l_boundary = 20;
+int h_boundary = 40;
+int center_pos = 25;
 
 void setup()
 {
   Serial.begin(9600);
   myServo.attach(servoPin);
 
-  myServo.write(centerPos);
+  myServo.write(center_pos);
 }
 
 void loop()
@@ -33,25 +35,32 @@ void loop()
       Serial.println(incomingData, DEC);
 
       intensity = incomingData * 3;
-      speed = incomingData * -1 + 40;
+
+      
+      speed = incomingData * -1 + 20;
 
     }
   }
 
   if(incomingData < 1){
-    myServo.write(centerPos);
+    myServo.write(center_pos);
   } else {
-    for(pos = 0; pos <= intensity; pos +=1){
+    for(pos = center_pos; pos <= h_boundary; pos += 1){
       myServo.write(pos);
       delay(speed);
     }
 
-    for(pos=intensity; pos >= 0; pos -= 1){
+    for(pos=h_boundary; pos >= l_boundary; pos -= 1){
+      myServo.write(pos);
+      delay(speed);
+    }
+
+    for(pos=l_boundary; pos <= center_pos; pos += 1){
       myServo.write(pos);
       delay(speed);
     }
 
     // Wait a little in between movements
-    delay(10000);
+    delay(2000);
   }
 }
